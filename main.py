a347c41ec97f9b80d111cda77d9744ef99c9ba65
Pratik <pratikkumar@gmail.com>
2022-03-07 23:22:07 +0530

    finalcommit

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
 import turtle
-import pandas as pd
+import pandas
 
 screen = turtle.Screen()
-screen.title('Us States Game')
-
+screen.title("U.S. States Game")
 image = "blank_states_img.gif"
-
 screen.addshape(image)
-screen.setup(height=491, width=725)
 turtle.shape(image)
 
-df = pd.read_csv('50_states.csv')
-all_states = df.state.to_list()
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-answer_state = screen.textinput(
-    title="Guess the State", prompt='What\'s another state name?')
-print(answer_state)
-
-while len(answer_state) < 50 :
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
+        guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
-        state_data = df[df.state == answer_state]
+        state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
-        t.write(state_data.state.item())
-
-
-# def get_mouse_click_coordinates(x,y):
-#     print(x,y)
-
-# turtle.onscreenclick(get_mouse_click_coordinates)
-
-# turtle.mainloop()
-screen.exitonclick()
+        t.write(answer_state)
